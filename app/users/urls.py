15c4cd47e1eab5be42594ac30cd7547c45c8939f
mrrahbarnia <mrrahbarnia@gmail.com>
2024-02-19 02:28:50 +0330

    JWT endpoints...

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from . import apis
 
 app_name = 'users'
 
 urlpatterns = [
-    path('registration/', apis.RegistrationApiView.as_view(), name='registration')
+    path('registration/', apis.RegistrationApiView.as_view(), name='registration'),
+    path('jwt/login/', TokenObtainPairView.as_view()),
+    path('jwt/refresh/', TokenRefreshView.as_view()),
+    path('jwt/verify/', TokenVerifyView.as_view())
 ]
