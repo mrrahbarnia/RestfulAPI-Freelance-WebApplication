96b551261699c0c8b66ba69b7541e80f2fe60d7d
mrrahbarnia <mrrahbarnia@gmail.com>
2024-02-19 17:34:11 +0330

    Test registration endpoint for creating profile.

--- app/core/tests/users/test_endpoints.py
+++ app/core/tests/users/test_endpoints.py
@@ -3,7 +3,10 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from users.models import BaseUser
+from users.models import (
+    BaseUser,
+    Profile
+)
 
 REGISTRATION_URL = reverse('users:registration')
 
@@ -16,12 +19,16 @@ class TestUserEndpoints(TestCase):
     def test_registration_endpoint_successfully(self):
         payload = {
             'phone_number': '09131111111',
+            'email': 'example@gmail.com',
             'password': '1234@example.com',
             'confirm_password': '1234@example.com',
         }
 
         response = self.client.post(REGISTRATION_URL, payload, format='json')
+        user_obj = BaseUser.objects.get(phone_number=payload['phone_number'])
 
+        self.assertTrue(Profile.objects.filter(user=user_obj).exists(), True)
+        self.assertEqual(Profile.objects.all().count(), 1)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['phone_number'], payload['phone_number'])
         self.assertTrue(
