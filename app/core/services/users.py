7fe0090ae6c85c436be72df77bf26421d59dd2fb
mrrahbarnia <mrrahbarnia@gmail.com>
2024-02-19 20:38:39 +0330

    Passed test.

--- app/core/services/users.py
+++ app/core/services/users.py
@@ -1,6 +1,7 @@
 """
 Layer for users services Business logics.
 """
+from PIL import Image as img
 from users.models import (
     BaseUser,
     Profile
@@ -11,10 +12,27 @@ def create_user(*, phone_number:str, password:str) -> BaseUser:
         phone_number=phone_number,
         password=password
     )
+    user.full_clean()
     return user
 
 def create_profile(*, user:BaseUser, email:str|None) -> Profile:
     return Profile.objects.create(user=user, email=email)
+
+def update_profile(
+        *, user:BaseUser, email:str|None, bio:str|None,
+        image:str|None, age:int|None, sex:str|None, city:str|None
+) -> Profile:
+    profile_obj = Profile.objects.get(user=user)
+
+    profile_obj.email = email
+    profile_obj.bio = bio
+    profile_obj.image = image
+    profile_obj.age = age
+    profile_obj.sex = sex
+    profile_obj.city = city
+
+    profile_obj.save()
+    return profile_obj
 
 def register(*, phone_number:str, email:str, password:str) -> BaseUser:
     user = create_user(phone_number=phone_number, password=password)
