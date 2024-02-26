7bd4f4257203a9a93d4524bbd481e47fbecc9875
mrrahbarnia <mrrahbarnia@gmail.com>
2024-02-26 10:48:03 +0330

    Passed tests

---
+++ app/core/services/skills.py
@@ -0,0 +1,11 @@
+from skill.models import (
+    Skill,
+    Category
+)
+
+
+def create_category(*, name:str) -> Category:
+    return Category.objects.create(name=name)
+
+def create_skill(*, name:str, category:Category) -> Skill:
+    return Skill.objects.create(name=name, category=category)
